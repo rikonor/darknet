926768640994803ef642a977b00a44350f7d450a
Or Rikon <rikonor@gmail.com>
2015-11-30 14:04:33 -0500

    Added fontawesome and styled desktop nav

--- vocativ-darknet-microsite/client/components/Nav.jsx
+++ vocativ-darknet-microsite/client/components/Nav.jsx
@@ -1,14 +1,16 @@
 Nav = React.createClass({
   render() {
     return (
-      <div className="nav">
-        <div className="logos">
-          <div className="logo vocativ"><a href="http://www.vocativ.com/"><img src="/images/vocativ-logo.svg" /></a></div>
-          <div className="logo darknet"><a href="/">DARKnet</a></div>
-        </div>
-        <div className="social">
-          <span>Facebook</span>
-          <span>Twitter</span>
+      <div className="nav-container">
+        <div className="nav">
+          <div className="logos">
+            <div className="logo vocativ"><a href="http://www.vocativ.com/"><img src="/images/vocativ-logo.svg" /></a></div>
+            <div className="logo darknet"><a href="/">DARKnet</a></div>
+          </div>
+          <div className="social">
+            <i className="fa fa-facebook"></i>
+            <i className="fa fa-twitter"></i>
+          </div>
         </div>
       </div>
     );
