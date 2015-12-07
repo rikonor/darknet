c3dc5ac18770118afb679c90810ae4d52a08da74
Or Rikon <rikonor@gmail.com>
2015-12-07 11:04:32 -0500

    Landing page styling fixes, small bug fix and more google analytics

--- vocativ-darknet-landing-page/js/main.js
+++ vocativ-darknet-landing-page/js/main.js
@@ -3,9 +3,30 @@ function trackClick(target) {
     hitType: 'event',
     eventCategory: 'DarkNet',
     eventAction: 'Click',
-    eventLabel: target
+    eventLabel: target,
+    eventValue: 1
   });
 }
+
+function trackSignup() {
+  ga('send', {
+    hitType: 'event',
+    eventCategory: 'DarkNet',
+    eventAction: 'Signup',
+    eventLabel: 'Vocativ Newsletter',
+    eventValue: 1
+  });
+}
+
+// showtime
+$(".vocativ-link").click(function() {
+  trackClick('Vocativ Link');
+});
+
+// vocativ
+$(".showtime-link").click(function() {
+  trackClick('Showtime Link');
+});
 
 $(".facebook-link").click(function() {
   trackClick('Facebook Link');
@@ -19,8 +40,15 @@ var signupForm = $(".signup-form form");
 var thankYouNote = $(".thank-you-invite");
 var inviteText = $(".invite-text");
 
-signupForm.submit(function() {
-  signupForm[0].reset();
+signupForm.submit(function(e) {
+  trackSignup();
+
+  // Clear the form (wait a sec for the form to submit)
+  setTimeout(function() {
+    signupForm[0].reset();
+  }, 1000);
+
+  // Hide the form and show the thank you note instead
   signupForm.parent().hide();
   inviteText.hide();
   thankYouNote.removeClass("hidden");
