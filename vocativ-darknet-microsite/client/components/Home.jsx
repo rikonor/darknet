3bd120b2996d3ef7df684c80660f57e36db3e674
Or Rikon <rikonor@gmail.com>
2015-12-14 10:34:49 -0500

    Some mobile-first scss fixes

--- vocativ-darknet-microsite/client/components/Home.jsx
+++ vocativ-darknet-microsite/client/components/Home.jsx
@@ -40,7 +40,18 @@ Home = React.createClass({
     return (
       <div className="home">
         <Page>
-          <Section><VideoEmbed youtubeId="pzN4WGPC4kc" /></Section>
+          <Section>
+            <VideoEmbed youtubeId="pzN4WGPC4kc" />
+
+            <div className="intro">
+              <div className="intro-header">
+                <div className="header">DARK NET</div>
+                <div className="subheader">from SHOWTIME + VOCATIV</div>
+              </div>
+              <div className="intro-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+            </div>
+          </Section>
+
           <Section><EpisodesLibraryLoader /></Section>
         </Page>
       </div>
