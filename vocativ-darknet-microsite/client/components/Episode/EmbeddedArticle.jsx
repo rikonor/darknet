6ca58f86e0a28831fad7517b158732201458332e
Or Rikon <rikonor@gmail.com>
2015-12-14 13:54:43 -0500

    Fix links on episode page

--- vocativ-darknet-microsite/client/components/Episode/EmbeddedArticle.jsx
+++ vocativ-darknet-microsite/client/components/Episode/EmbeddedArticle.jsx
@@ -2,11 +2,13 @@ EmbeddedArticle = React.createClass({
   render() {
     return (
       <div className="embedded-article">
-        <div className="image">
-          <a href={this.props.article.url}><img src={this.props.article.imageUrl()}></img></a>
-        </div>
-        <div className="category">{this.props.article.category}</div>
-        <div className="description">{this.props.article.description}</div>
+        <a href={this.props.article.url}>
+          <div className="image">
+            <img src={this.props.article.imageUrl()}></img>
+          </div>
+          <div className="category">{this.props.article.category}</div>
+          <div className="description">{this.props.article.description}</div>
+        </a>
       </div>
     );
   }
--- vocativ-darknet-microsite/client/components/Episode/EmbeddedVideo.jsx
+++ vocativ-darknet-microsite/client/components/Episode/EmbeddedVideo.jsx
@@ -2,11 +2,13 @@ EmbeddedVideo = React.createClass({
   render() {
     return (
       <div className="embedded-video">
-        <div className="image">
-          <div className="voc-video-overlay"></div>
-          <a href={this.props.video.url}><img src={this.props.video.imageUrl()}></img></a>
-        </div>
-        <div className="name">{this.props.video.name}</div>
+        <a href={this.props.video.url}>
+          <div className="image">
+            <div className="voc-video-overlay"></div>
+            <img src={this.props.video.imageUrl()}></img>
+          </div>
+          <div className="name">{this.props.video.name}</div>
+        </a>
       </div>
     );
   }
