03fba0aae2df044b0de8629bae8fbd8f440e2b74
Or Rikon <rikonor@gmail.com>
2015-12-14 16:44:46 -0500

    Footer done

--- vocativ-darknet-microsite/client/components/Footer.jsx
+++ vocativ-darknet-microsite/client/components/Footer.jsx
@@ -9,8 +9,8 @@ Footer = React.createClass({
         <div className="footer">
           <div className="menu">
             <span className="menu-item"><a href="/">Home</a></span>
-            <span className="menu-item"><a href="/about">About DARKnet</a></span>
-            <span className="menu-item"><a href="/legal">Legal</a></span>
+            <span className="menu-item"><a href="http://www.vocativ.com/about/terms-use/">Terms of Service</a></span>
+            <span className="menu-item"><a href="http://www.vocativ.com/legal/">Privacy Policy</a></span>
           </div>
           <div className="copyright">&copy; Copyright Vocativ {this.currentYear()}</div>
         </div>
