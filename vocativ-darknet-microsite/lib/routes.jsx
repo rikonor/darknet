41056adf9bd4aa68f581e6232f4d076292aac44a
Or Rikon <rikonor@gmail.com>
2015-12-21 12:33:24 -0500

    Track routes

--- vocativ-darknet-microsite/lib/routes.jsx
+++ vocativ-darknet-microsite/lib/routes.jsx
@@ -1,17 +1,20 @@
 FlowRouter.route('/', {
   action() {
+    GAnalytics.pageview("/");
     ReactLayout.render(HomeLayout, { content: <HomeLoader /> });
   }
 });
 
 FlowRouter.route('/episodes/:episodeName', {
   action(params, queryParams) {
+    GAnalytics.pageview(`/episodes/${params.episodeName}`);
     ReactLayout.render(MainLayout, { content: <EpisodeLoader episodeName={params.episodeName}/> });
   }
 });
 
 FlowRouter.route('/about', {
   action() {
+    GAnalytics.pageview("/about");
     ReactLayout.render(MainLayout, { content: <About /> });
   }
 });
