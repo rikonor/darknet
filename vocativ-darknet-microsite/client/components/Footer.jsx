000f195585a0d0166dc679f286657e3eb5710eb1
Or Rikon <rikonor@gmail.com>
2015-12-21 16:22:44 -0500

    Added signup form

--- vocativ-darknet-microsite/client/components/Footer.jsx
+++ vocativ-darknet-microsite/client/components/Footer.jsx
@@ -7,6 +7,7 @@ Footer = React.createClass({
     return (
       <div className="footer-container">
         <div className="footer">
+          <NewsletterSignup />
           <div className="menu">
             <span className="menu-item"><a href="/">Home</a></span>
             <span className="menu-item"><a href="http://www.vocativ.com/about/terms-use/" target="_blank" onClick={createEventTracker('terms-of-use-footer-btn')}>Terms of Service</a></span>
@@ -16,5 +17,47 @@ Footer = React.createClass({
         </div>
       </div>
     );
+  }
+});
+
+var NewsletterSignup = React.createClass({
+  getInitialState() {
+    return {
+      submitted: false
+    };
+  },
+
+  handleSubmit() {
+    console.log("reached");
+    this.setState({submitted: true});
+  },
+
+  render() {
+    let tmpStyles = {
+      position: 'absolute',
+      left: '-5000px'
+    };
+
+    if (! this.state.submitted) {
+      return (
+        <div className="signup-form">
+          <form action="//vocativ.us3.list-manage.com/subscribe/post?u=4ffa741e7ab8a14a6212cbd45&amp;id=a5ec73e244" method="post" target="_blank" onSubmit={this.handleSubmit}>
+            <input type="text" name="EMAIL" id="mce-EMAIL" placeholder="Email Address" />
+            <button>Sign Up</button>
+
+            <div style={tmpStyles} aria-hidden="true">
+              <input type="text" name="b_4ffa741e7ab8a14a6212cbd45_a5ec73e244" tabIndex="-1" value="" />
+            </div>
+          </form>
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="signup-form">
+          Thank you for submitting me!
+        </div>
+      );
+    }
   }
 });
