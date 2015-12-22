92bd8c9285dc984231ca2ddc4d4493ca3aaf0fea
Or Rikon <rikonor@gmail.com>
2015-12-22 14:22:22 -0500

    DataViz should open to a lightbox

--- vocativ-darknet-microsite/client/components/DataViz.jsx
+++ vocativ-darknet-microsite/client/components/DataViz.jsx
@@ -1,8 +1,44 @@
+const ACTIVE = "active";
+const HIDDEN = "hidden";
+
 DataVisualization = React.createClass({
+  getInitialState() {
+    return {
+      state: HIDDEN
+    };
+  },
+
+  closeLightbox() {
+    if (this.state.state === ACTIVE) {
+      this.setState({state: HIDDEN});
+    }
+  },
+
+  openLightbox() {
+    if (this.state.state === HIDDEN) {
+      this.setState({state: ACTIVE});
+    }
+  },
+
+  handleClick() {
+    this.openLightbox();
+    GAnalytics.event("DataViz", "show", this.props.dataviz.title);
+  },
+
   render() {
+    let datavizLightbox = null;
+    if (this.state.state === ACTIVE) {
+      datavizLightbox = (
+        <Lightbox onClose={this.closeLightbox}>
+          <Image imageUrl={this.props.dataviz.imageUrl()} />
+        </Lightbox>
+      );
+    }
+
     return (
-      <div className="dataviz">
+      <div className="dataviz" onClick={this.handleClick}>
         <Image imageUrl={this.props.dataviz.imageUrl()} />
+        {datavizLightbox}
       </div>
     );
   }
