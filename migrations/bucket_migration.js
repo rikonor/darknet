64fd20ebd277b7bbbaac97805d8ff0d6ccc1c7a5
Or Rikon <rikonor@gmail.com>
2016-01-15 10:21:58 -0500

    Style change

--- migrations/bucket_migration.js
+++ migrations/bucket_migration.js
@@ -2,12 +2,25 @@
 
 var images = Images.find().fetch();
 
-function convertImage(image) {
-  var OLD_BUCKET = "vocativ-darknet-images-bucket-development";
-  var NEW_BUCKET = "vocativ-darknet-test";
+var OLD_BUCKET = "vocativ-darknet-images-bucket";
+var NEW_BUCKET = "vocativ-darknet";
 
+function convertImage(image) {
   var newImageUrl = image.image.replace(OLD_BUCKET, NEW_BUCKET);
+  // console.log(newImageUrl);
   Images.update({_id: image._id}, {$set: { image: newImageUrl}});
 }
 
 images.forEach(convertImage);
+
+// Test it worked
+
+var images = Images.find().fetch();
+
+function testImage(image) {
+  if (image.image.indexOf(NEW_BUCKET) === -1) {
+    console.log("Failed for", image._id, image.image);
+  }
+}
+
+images.forEach(testImage);
